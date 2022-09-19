d4922010b620e4772f435943548a214583381d51
mugiwarafx <sangalesbenes@uoc.edu>
2022-09-19 13:08:54 +0200

    backend v1.0.0

---
+++ backend/README.md
@@ -0,0 +1,12 @@
+# mvpfs app
+
+## with node
+git clone {this-repo}
+npm install 
+npm start
+
+
+## with docker
+git clone {this-repo}
+docker build -t {your-image-name} .
+docker run -p 5000:5000 {your-image-name} 
---
+++ backend/data/queries/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO products (seller_id, name, amount_available, cost, uploaded_at) VALUES (1, 'product name 1', 1000, 10, current_timestamp);
+INSERT INTO products (seller_id, name, amount_available, cost, uploaded_at) VALUES (1, 'product name 2', 2000, 20, current_timestamp);
+INSERT INTO products (seller_id, name, amount_available, cost, uploaded_at) VALUES (1, 'product name 3', 3000, 30, current_timestamp);
+INSERT INTO products (seller_id, name, amount_available, cost, uploaded_at) VALUES (1, 'product name 4', 4000, 40, current_timestamp);
+INSERT INTO products (seller_id, name, amount_available, cost, uploaded_at) VALUES (1, 'product name 5', 5000, 50, current_timestamp);
+
+
+
+INSERT INTO users (username, password, role, deposite, created_at) VALUES ('username_1', 'test', 'seller', 0, CURRENT_TIMESTAMP);
+INSERT INTO users (username, password, role, deposite, created_at) VALUES ('username_2', 'test', 'buyer', 100, CURRENT_TIMESTAMP);
+INSERT INTO users (username, password, role, deposite, created_at) VALUES ('username_3', 'test', 'seller', 0, CURRENT_TIMESTAMP);
+INSERT INTO users (username, password, role, deposite, created_at) VALUES ('username_4', 'test', 'buyer', 100, CURRENT_TIMESTAMP);
+INSERT INTO users (username, password, role, deposite, created_at) VALUES ('username_5', 'test', 'seller', 0, CURRENT_TIMESTAMP);
